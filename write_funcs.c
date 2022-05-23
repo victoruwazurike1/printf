bfe8f038ad85f91027d3dac772b050cc9136851b
Uwazurike Victor <victoru779@gmail.com>
2022-05-23 15:51:52 +0100

    modified write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -3,11 +3,11 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: This is the character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
+ * less frequently
  */
 int _putchar(char c)
 {
